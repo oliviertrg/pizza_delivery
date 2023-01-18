0fc77df1170f8ad2265f089d5921403385992a95
oliviertrg <102872105+oliviertrg@users.noreply.github.com>
2023-01-18 20:25:51 +0700

    fix middleware

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ app.add_middleware(
     CORSMiddleware ,
     allow_origins = origins ,
     allow_credentials = True ,
-    allow_methods = ["http://127.0.0.1:8000/"] ,
-    allow_headers = ["http://127.0.0.1:8000/"]
+    allow_methods = ["*"] ,
+    allow_headers = ["*"]
 )
 
 app.include_router(users.router)
